109a91ca06de9c7c3bda14fa92a72279a69b101d
Mark Kolich <social@kolich.com>
2013-03-09 15:12:32 -0800

    changing build to use kolich-common 0.0.6

--- project/Build.scala
+++ project/Build.scala
@@ -31,7 +31,7 @@ import com.typesafe.sbteclipse.plugin.EclipsePlugin._
 
 object Dependencies {
 
-  private val kolichCommon = "com.kolich" % "kolich-common" % "0.0.4" % "compile"
+  private val kolichCommon = "com.kolich" % "kolich-common" % "0.0.6" % "compile"
 
   val deps = Seq(kolichCommon)
 
@@ -51,7 +51,7 @@ object Spring extends Build {
   import Resolvers._
 
   private val aName = "kolich-bolt"
-  private val aVer = "0.0.3"
+  private val aVer = "0.0.4"
   private val aOrg = "com.kolich"
 
   lazy val bolt: Project = Project(
